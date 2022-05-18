4add5740d16ce3c0fa063fa485614df7ececb1d8
JOEJIBII <Wutwanich.dev@gmail.com>
2022-05-19 02:13:49 +0700

    Change Path Config

--- API GATEWAY/configs/connection_mongodb.js
+++ API GATEWAY/configs/connection_mongodb.js
@@ -1,5 +1,6 @@
 const  { MongoClient } = require("mongodb");
-const {CONN,DB}  = require("/API/Config/db-connection");
+const {CONN,DB}  = require("./../../Config/db-connection");
+
 module.exports = () => {
     return new Promise(async (resolve, reject) => {
         MongoClient.connect(CONN, { useNewUrlParser: true, useUnifiedTopology: true })
