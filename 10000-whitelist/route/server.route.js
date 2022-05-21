2de0c1cfe1c51d0935c37ed911ed6454d84363f0
JOEJIBII <Wutwanich.dev@gmail.com>
2022-05-21 20:20:07 +0700

    update

--- 10000-whitelist/route/server.route.js
+++ 10000-whitelist/route/server.route.js
@@ -1,6 +1,8 @@
 const controller = require('../controllers/server.controller')
 
 module.exports = function(app) {
+    app.post('/', controller.checkagent);
     app.post('/whitelist', controller.checkagent);
+    
     ///getuserprofile/?username=value
 }
