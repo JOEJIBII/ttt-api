a0d6bf7bc110240caeebae44b5b0e22c40c01cda
JOEJIBII <Wutwanich.dev@gmail.com>
2022-06-06 23:32:37 +0700

    login

--- 11005-Login-member/controllers/server.controller.js
+++ 11005-Login-member/controllers/server.controller.js
@@ -41,10 +41,11 @@ module.exports.login = async function (req, res) {
                  }else{
                      const inst_token =  await model.inserttoken(ResultToken).catch(() => { throw err });
                      if(inst_token.insertedId){
+                        let responses = await functions.Mappingdata(ResultToken,credit.result.result.data.balance).catch(() => {throw err});
                              res.send({
                                  status: "200",
                                  message: "success",
-                                 ResultToken
+                                 result:responses
                  
                              }).end();
                          }else
