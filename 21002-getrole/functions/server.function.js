d8302e49bd800220b7c86159edaf9300136c446f
JOEJIBII <Wutwanich.dev@gmail.com>
2022-06-11 02:39:00 +0700

    getrole 20220610

---
+++ 21002-getrole/functions/server.function.js
@@ -0,0 +1,39 @@
+const { MongoDB } = require('../configs/connection_mongodb');
+const moment = require('moment');
+const collectionhistory_log_api ="history_log_api"
+
+module.exports.logs = (body,ip) => {
+    return new Promise(async (resolve, reject) => {
+        await MongoDB.collection(collectionhistory_log_api)
+        .insertOne({
+            memb_id:body.agent_id,
+            api_name:"21002-getrole",
+            ip_address:ip,
+            create_date:moment().format()
+        })
+            .then(result => resolve(result))
+            .catch(error => reject(error));
+    });
+}
+
+module.exports.Mappingdata = async (roles) => {
+    return new Promise(async (resolve) => {
+        let role = []
+        //console.log("member",Resulttoken)
+        roles.forEach(e => {
+                role = role.concat(e)
+        })
+        //console.log(memb)
+        resolve({ 
+        "role": role.map(e => {
+                return {
+                    _id:Object(e._id),
+                    role: e.name,
+                    description: e.description
+                }
+            
+            
+        })
+        },)
+    })
+    }
