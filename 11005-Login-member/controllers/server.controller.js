b27e915041a25d4a4bd27215d37b9bdad2d04cc7
JOEJIBII <Wutwanich.dev@gmail.com>
2022-06-23 03:39:38 +0700

    login

--- 11005-Login-member/controllers/server.controller.js
+++ 11005-Login-member/controllers/server.controller.js
@@ -6,55 +6,53 @@ const functions = require('../functions/server.function');
 //const _ = require("lodash");
 module.exports.login = async function (req, res) {
     res.setHeader('Content-Type', 'application/json');
-//console.log("payload",JSON.parse(req.headers.payload))
-//console.log("headers",req.headers)
-//console.log(req.headers['user-agent'])
+    //console.log("payload",JSON.parse(req.headers.payload))
+    //console.log("headers",req.headers)
+    //console.log(req.headers['user-agent'])
     try {
-        let ResultToken = await model.login(req.body,req.headers).catch(() => { throw err });
+        let ResultToken = await model.login(req.body, req.headers).catch(() => { throw err });
         console.log(ResultToken)
         //console.log("Test",ResultToken.profile_mem._id)
         if (ResultToken.length !== 0) {
-             console.log('Result',ResultToken)
-             let Token = model.gen_token(ResultToken)
+            console.log('Result', ResultToken)
+            let Token = model.gen_token(ResultToken)
             const credit = await functions.ProfilePD(ResultToken[0].username).catch(() => { throw err });
             //console.log(Token)
-            if(credit.result.status === "200"){
+            if (credit.result.status === "200") {
                 const find = await model.find_session(ResultToken[0]._id).catch(() => { throw err });
                 // console.log(find[0]._id)
-                 if(find.length > 0){
-                     const remove =   await model.remove_session(find[0]._id).catch(() => { throw err }); 
-                     if (remove.deletedCount > 0){
-                         const inst_token =  await model.inserttoken(ResultToken,Token).catch(() => { throw err });
-                         if(inst_token.insertedId){
-                            let responses = await functions.Mappingdata(ResultToken,credit.result.result.data.balance,Token).catch(() => {throw err});
+                if (find.length > 0) {
+                    const remove = await model.remove_session(find[0]._id).catch(() => { throw err });
+                    if (remove.deletedCount > 0) {
+                        const inst_token = await model.inserttoken(ResultToken, Token).catch(() => { throw err });
+                        if (inst_token.insertedId) {
+                            let responses = await functions.Mappingdata(ResultToken, credit.result.result.data.balance, Token).catch(() => { throw err });
                             console.log(responses)
-                                 res.send({
-                                     status: "200",
-                                     message: "success",
-                                     result:responses
-                                     //credit:credit.result.result.data.balance
-                                 }).end();
-                             }else
-                             {
-                                 res.send({ status: "201", message: 'Cannot Create Token Please try again' }).end();
-                             }
-                     }
-                 }else{
-                     const inst_token =  await model.inserttoken(ResultToken).catch(() => { throw err });
-                     if(inst_token.insertedId){
-                        let responses = await functions.Mappingdata(ResultToken,credit.result.result.data.balance).catch(() => {throw err});
-                             res.send({
-                                 status: "200",
-                                 message: "success",
-                                 result:responses
-                 
-                             }).end();
-                         }else
-                         {
-                             res.send({ status: "201", message: 'Cannot Create Token Please try again' }).end();
-                         }
-                 }
-            }else{
+                            res.send({
+                                status: "200",
+                                message: "success",
+                                result: responses
+                                //credit:credit.result.result.data.balance
+                            }).end();
+                        } else {
+                            res.send({ status: "201", message: 'Cannot Create Token Please try again' }).end();
+                        }
+                    }
+                } else {
+                    const inst_token = await model.inserttoken(ResultToken).catch(() => { throw err });
+                    if (inst_token.insertedId) {
+                        let responses = await functions.Mappingdata(ResultToken, credit.result.result.data.balance).catch(() => { throw err });
+                        res.send({
+                            status: "200",
+                            message: "success",
+                            result: responses
+
+                        }).end();
+                    } else {
+                        res.send({ status: "201", message: 'Cannot Create Token Please try again' }).end();
+                    }
+                }
+            } else {
                 res.send({ credit }).end();
             }
         } else {
