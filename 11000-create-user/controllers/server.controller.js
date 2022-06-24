4e848c052b42b6ef82f018370b4620f420b40a84
JOEJIBII <Wutwanich.dev@gmail.com>
2022-06-25 02:49:37 +0700

    update web

--- 11000-create-user/controllers/server.controller.js
+++ 11000-create-user/controllers/server.controller.js
@@ -4,86 +4,76 @@ const model = require('../models/server.model');
 const functions = require('../functions/server.function');
 
 
-module.exports.registermember = async function (req,res) {
+module.exports.registermember = async function (req, res) {
     res.setHeader('Content-type', 'application/json');
     let body = req.body.body
-    try{
-        // const CONF = await model.findConF(body).catch(() => {throw err});
-        // const regis = await functions.registermemberPD2(CONF).catch(() => {throw err});
-        // console.log(regis)
-        // console.log(err)
+    try {
         req.headers["x-real-ip"]
         console.log(body)
-        let Ckbank = await model.CheckBankAccount(body).catch(() => {throw err});
-        console.log(Ckbank.length)
-        let Cktel = await model.CheckTel(body).catch(() => {throw err});
-        
-        const verifyCap = await functions.verifycaptcha(body.captchaID,body.value).catch(() => {throw err});
-       // console.log(err)
-       // console.log(verifyCap)
-        if(verifyCap.status === "200"){
-            if(Cktel.length == 0 && Ckbank.length == 0){
-                const CONF = await model.findConF(body).catch(() => {throw err});
-               if(CONF.value !== null){
-                //console.log(CONF); insertbankmemb
-                const userTemp = CONF.value.provider.prov_agentusername + CONF.value.prefix + CONF.value.member.running_number
-                //console.log(userTemp);
-                   let Result = await model.register(body,req.headers.host,CONF.value.prefix + CONF.value.member.running_number ).catch(() => {throw err});
-                   if(Result.insertedId !== null && Result.insertedId !== '') {
-                       // let insertbank = await model.insertbankmemb(Result.insertedId,body).catch(() => {throw err});
-                       await model.insertbankmemb(Result.insertedId,body).catch(() => {throw err});
-                        const createacct = await model.createaccountprovider(body,Result.insertedId,CONF,userTemp).catch(() => {throw err});
-                        const acct_pd = await model.get_acct_pd(createacct.insertedId).catch(() => {throw err});
-                        //console.log(acct_pd)
-                        const log = await functions.logs(body,Result.insertedId,req.headers.host).catch(() => {throw err});
-                        const regis = await functions.registermemberPD(CONF,userTemp).catch(() => {throw err});
-                         console.log("regis",regis)
-                        if(regis.result.code === 0){
+        //if(payload.user_id === "" || payloa)
+        const verifyCap = await functions.verifycaptcha(body.captchaID, body.value).catch(() => { throw err });
+        if (verifyCap.status === "200") {
+            let Ckbank = await model.CheckBankAccount(body).catch(() => { throw err });
+            console.log(Ckbank.length)
+            let Cktel = await model.CheckTel(body).catch(() => { throw err });
+            if (Cktel.length == 0 && Ckbank.length == 0) {
+                const CONF = await model.findConF(body).catch(() => { throw err });
+                if (CONF.value !== null) {
+                    const userTemp = CONF.value.provider.prov_agentusername + CONF.value.prefix + CONF.value.member.running_number
+                    let Result = await model.register(body, req.headers.host, CONF.value.prefix + CONF.value.member.running_number).catch(() => { throw err });
+                    if (Result.insertedId !== null && Result.insertedId !== '') {
+                        await model.insertbankmemb(Result.insertedId, body).catch(() => { throw err });
+                        const createacct = await model.createaccountprovider(body, Result.insertedId, CONF, userTemp).catch(() => { throw err });
+                        await model.get_acct_pd(createacct.insertedId).catch(() => { throw err });
+                        await functions.logs(body, Result.insertedId, req.headers.host).catch(() => { throw err });
+                        const regis = await functions.registermemberPD(CONF, userTemp).catch(() => { throw err });
+                        console.log("regis", regis)
+                        if (regis.result.code === 0) {
                             res.send({
                                 status: "200",
                                 Username: body.tel,
                                 Password: regis.Password,
-                                username_prov : regis.acct,
+                                username_prov: regis.acct,
                                 message: "success"
                             }).end();
-                        }else{
+                        } else {
                             console.log(createacct.insertedId)
                             console.log(Result.insertedId)
-                            const delete_mem = await model.removemember(Result.insertedId).catch(() => {throw err});
+                            const delete_mem = await model.removemember(Result.insertedId).catch(() => { throw err });
                             console.log(delete_mem)
-                            if(delete_mem.deletedCount === 1){
-                                const delete_mem_pd = await model.removememberpd(createacct.insertedId).catch(() => {throw err});
+                            if (delete_mem.deletedCount === 1) {
+                                const delete_mem_pd = await model.removememberpd(createacct.insertedId).catch(() => { throw err });
                                 console.log(delete_mem_pd)
                             }
-                            res.send({result:regis.result }).end();
+                            res.send({ result: regis.result }).end();
                         }
-                        
-                   }else{
-                       res.send({ status: "201", message: "Cannot Insert data" }).end();
-                   }
-               }else{
-                   res.send({ status: "202", message: "Not Found domain" }).end();
-               }
-               
-           }else{
-                if(Cktel.length !== 0   &&  Ckbank.length !== 0 ){
+
+                    } else {
+                        res.send({ status: "201", message: "Cannot Insert data" }).end();
+                    }
+                } else {
+                    res.send({ status: "202", message: "Not Found domain" }).end();
+                }
+
+            } else {
+                if (Cktel.length !== 0 && Ckbank.length !== 0) {
                     res.send({
                         status: "203",
-                        bank_account : 
-                            {
-                                bank_acct:body.bank_acct,
+                        bank_account:
+                        {
+                            bank_acct: body.bank_acct,
 
-                            },
+                        },
                         tel: body.tel,
                         message: "PhoneNumber และ BankAccount มีอยู่ในระบบแล้ว"
                     }).end();
-                }else{
-                    if(Cktel.length == 0  && Ckbank.length !== 0){
+                } else {
+                    if (Cktel.length == 0 && Ckbank.length !== 0) {
                         res.send({
                             status: "204",
-                            bank_account : 
+                            bank_account:
                             {
-                                bank_acct:body.bank_acct,
+                                bank_acct: body.bank_acct,
 
                             },
                             // bank_account: body.banking_account.map(e => 
@@ -97,8 +87,8 @@ module.exports.registermember = async function (req,res) {
                             // ),
                             message: "BankAccount มีอยู่ในระบบแล้ว"
                         }).end();
-                    }else{
-                        if(Cktel.length !== 0  && Ckbank.length == 0){
+                    } else {
+                        if (Cktel.length !== 0 && Ckbank.length == 0) {
                             res.send({
                                 status: "205",
                                 tel: body.tel,
@@ -107,19 +97,19 @@ module.exports.registermember = async function (req,res) {
                         }
                     }
                 }
-                
+
             }
-        }else{
+        } else {
             res.send({
-                status:verifyCap.status,
-                message:verifyCap.message
+                status: verifyCap.status,
+                message: verifyCap.message
             }).end();
         }
-            
-        }catch (error){
-            //console.log(error);
-            res.send({ status: "400", message: error}).end();
-        }
+
+    } catch (error) {
+        //console.log(error);
+        res.send({ status: "400", message: error }).end();
+    }
 
 }
 
