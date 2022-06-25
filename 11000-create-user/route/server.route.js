bd2a14241cd8eb601b503379ebceb00cf0997e02
JOEJIBII <Wutwanich.dev@gmail.com>
2022-06-25 17:12:08 +0700

    createmember

--- 11000-create-user/route/server.route.js
+++ 11000-create-user/route/server.route.js
@@ -2,4 +2,5 @@ const controller = require('../controllers/server.controller')
 
 module.exports = function(app) {
     app.post('/register', controller.registermember);
+    app.post('/', controller.registermember);
 }
