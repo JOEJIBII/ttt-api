6a2ccb6dbf8f857f4182ee8792d2b638beb85ea6
JOEJIBII <Wutwanich.dev@gmail.com>
2022-07-06 01:56:45 +0700

    update to Teatrueteam

--- 21022-create-user/controllers/server.controller.js
+++ 21022-create-user/controllers/server.controller.js
@@ -14,7 +14,7 @@ module.exports.registermember = async function (req, res) {
             const CONF = await model.findConF(body).catch(() => { throw err });
             if (CONF.value !== null) {
                 const userTemp = CONF.value.provider.prov_agentusername + CONF.value.prefix + CONF.value.member.running_number
-                let Result = Result = await model.register(body, req.headers.host, CONF.value.prefix + CONF.value.member.running_number).catch(() => { throw err });
+                let Result = await model.register(body, req.headers.host, CONF.value.prefix + CONF.value.member.running_number).catch(() => { throw err });
                 if (Result.insertedId !== null && Result.insertedId !== '') {
                     let membbank = await model.insertbankmemb(Result.insertedId, body).catch(() => { throw err });
                     console.log("bank", membbank)
