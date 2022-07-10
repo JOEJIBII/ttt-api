9497aca103118bcddd7453ee6b19812c42f008ce
JOEJIBII <Wutwanich.dev@gmail.com>
2022-07-10 20:34:55 +0700

    push msg

---
+++ 50000-api-get-data-pushbullet/controllers/functions/server.function.js
@@ -0,0 +1,11 @@
+
+module.exports.connectSocketIO = async data => {
+    return new Promise((resolve, reject) => {
+        try {
+
+            resolve();
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
