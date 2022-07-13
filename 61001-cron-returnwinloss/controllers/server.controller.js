642d4a8d7bb8a463efce0f3d573a1dcb648fb14a
JOEJIBII <Wutwanich.dev@gmail.com>
2022-07-14 02:06:37 +0700

    update

--- 61001-cron-returnwinloss/controllers/server.controller.js
+++ 61001-cron-returnwinloss/controllers/server.controller.js
@@ -13,7 +13,7 @@ module.exports = async () => {
     new CronJob("*/10 * * * * *", async () => {
         try {
             console.log("start")
-            
+            working === false
              if (working !== false) {
                 console.log("working")
                 working === false
