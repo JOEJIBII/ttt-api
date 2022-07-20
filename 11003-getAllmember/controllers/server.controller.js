7ae306fbc6fcb7f7dd8d09e72aafde53d1fbeff0
JOEJIBII <Wutwanich.dev@gmail.com>
2022-07-21 02:59:05 +0700

    update all member

--- 11003-getAllmember/controllers/server.controller.js
+++ 11003-getAllmember/controllers/server.controller.js
@@ -6,29 +6,46 @@ module.exports.getallmember = async function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     console.log(JSON.parse(req.headers.payload))
     try {
-
-        let ResultMEMBER = await model.getallmember(req.body).catch(() => { throw err });
-        let page_option = (await model.page_option(req.body).catch(() => { throw err }))[0];
-         console.log(ResultMEMBER)
-        if (ResultMEMBER && ResultMEMBER.length) {
-            // console.log('Result',ResultMEMBER)
-            const log = await functions.logs(req.body, req.headers.host).catch(() => { throw err });
-            res.send({
-                status: "200",
-                message: "success",
-                result: {
-                    total: ResultMEMBER.length,
-                    page_option,
-                    Member: ResultMEMBER,
-
-                }
-            }).end();
-        } else {
-            res.send({ status: "201", message: 'not found data' }).end();
+        if(req.body?.page === true && req.body?.range){
+            let ResultMEMBER = await model.getallmember(req.body).catch(() => { throw err });
+            let page_option = (await model.page_option(req.body).catch(() => { throw err }))[0];
+            if (ResultMEMBER && ResultMEMBER.length) {
+                // console.log('Result',ResultMEMBER)
+                const log = await functions.logs(req.body, req.headers.host).catch(() => { throw err });
+                res.send({
+                    status: "200",
+                    message: "success",
+                    result: {
+                        total: ResultMEMBER.length,
+                        page_option,
+                        Member: ResultMEMBER,
+    
+                    }
+                }).end();
+            } else {
+                res.send({ status: "201", message: 'not found data' }).end();
+            }
+        }else{
+            let ResultMEMBER = await model.getallmemb(req.body).catch(() => { throw err });
+            console.log(ResultMEMBER)
+            if (ResultMEMBER && ResultMEMBER.length) {
+                // console.log('Result',ResultMEMBER)
+                const log = await functions.logs(req.body, req.headers.host).catch(() => { throw err });
+                res.send({
+                    status: "200",
+                    message: "success",
+                    result: {
+                        total: ResultMEMBER.length,
+                      //  page_option,
+                        Member: ResultMEMBER,
+    
+                    }
+                }).end();
+            } else {
+                res.send({ status: "201", message: 'not found data' }).end();
+            }
         }
-
-
-
+        //let ResultMEMBER = await model.getallmember(req.body).catch(() => { throw err });
     } catch (error) {
         console.error(error);
         res.send({ status: "300", message: 'internal error' }).end();
--- 11003-getAllmember/models/server.model.js
+++ 11003-getAllmember/models/server.model.js
@@ -177,6 +177,177 @@ module.exports.getallmember = (body) => {
     });
 }
 
+module.exports.getallmemb = (body) => {
+    // console.log(body);
+    // let skip = 0;
+    // skip = (Number(body.page) - 1) * Number(body.range);
+    // console.log(skip, Number(body.range) + skip)
+    return new Promise(async (resolve, reject) => {
+        await MongoDB.collection(collectionmember)
+            .aggregate([
+                {
+                    $match: {
+                        $and: [
+                            {
+                                agent_id: ObjectId(body.agent_id)
+                            }
+
+                        ]
+                    }
+                },
+                {
+                    $project: {
+                        _id: 1,
+                        username: "$username",
+                        agent_id: "$agent_id",
+                        line_id: "$line_id",
+                        profile: {
+                            name: "$name",
+                            surename: "$surname",
+                            birthday_date: "$birthday",
+                            tel: "$tel",
+                            privilege: "$member_profile.memb_privilege",
+                            channel: "$channel",
+                            partner: "$member_profile.memb_partner",
+                            note: "$remark"
+                        },
+                        banking_account: "$banking_account",
+                        financial: "$financial",
+                        status: "$status",
+                        status_newmember: "$status_new_member",
+                        create_date: "$cr_date",
+                        update_date: "$upd_date",
+                        update_by: "$upd_by"
+
+                    }
+                },
+                {
+                    $lookup: {
+                        from: "member_provider_account",
+                        localField: "_id",
+                        foreignField: "memb_id",
+                        as: "provideracct"
+                    }
+                },
+                {
+                    $unwind: { path: "$provideracct" }
+                },
+                {
+                    $project: {
+                        _id: 1,
+                        username: "$username",
+                        agent_id: "$agent_id",
+                        webname: "$name",
+                        line_id: "$line_id",
+                        username_provider: "$provideracct.username",
+                        profile: "$profile",
+                        banking_account: "$banking_account",
+                        financial: "$financial",
+                        status: "$status",
+                        status_newmember: "$status_newmember",
+                        create_date: "$create_date",
+                        update_date: "$update_date",
+                        update_by: "$update_by",
+                    }
+                }, {
+                    $lookup: {
+                        from: "agent",
+                        localField: "agent_id",
+                        foreignField: "_id",
+                        as: "Tbagent"
+                    }
+                }, {
+                    $unwind: { path: "$Tbagent" }
+                },
+                {
+                    $project: {
+                        _id: 1,
+                        prefix: "$Tbagent.name",
+                        username: "$username",
+                        //prefix: "$Tbagent.name",
+                        line_id: "$line_id",
+                        username_provider: "$username_provider",
+                        profile: "$profile",
+                        banking_account: "$banking_account",
+                        financial: "$financial",
+                        status: "$status",
+                        status_newmember: "$status_newmember",
+                        create_date: "$create_date",
+                        update_date: "$update_date",
+                        update_by: "$update_by",
+                    }
+                }, {
+                    $lookup: {
+                        from: "memb_bank_account",
+                        localField: "_id",
+                        foreignField: "memb_id",
+                        as: "bank"
+                    }
+                }, {
+                    $unwind: { path: "$bank" }
+                }, {
+                    $project: {
+                        _id: 1,
+                        prefix: "$prefix",
+                        username: "$username",
+                        //prefix: "$Tbagent.name",
+                        line_id: "$line_id",
+                        username_provider: "$username_provider",
+                        profile: "$profile",
+                        bank_id: "$bank.bank_id",
+                        banking_account: { account_bank_id: "$bank._id", account_number: "$bank.account_number", account_name: "$bank.account_name" },
+                        financial: "$financial",
+                        status: "$status",
+                        status_newmember: "$status_newmember",
+                        create_date: "$create_date",
+                        update_date: "$update_date",
+                        update_by: "$update_by",
+                    }
+                },
+                {
+                    $lookup: {
+                        from: "bank",
+                        localField: "bank_id",
+                        foreignField: "_id",
+                        as: "banking"
+                    }
+                }, {
+                    $unwind: { path: "$banking" }
+                }, {
+                    $project: {
+                        _id: 1,
+                        prefix: "$prefix",
+                        username: "$username",
+                        //prefix: "$prefix",
+                        line_id: "$line_id",
+                        username_provider: "$username_provider",
+                        profile: "$profile",
+                        banking_account: { bank_id: "$banking._id", account_bank_id: "$banking_account.account_bank_id", account_number: "$banking_account.account_number", account_name: "$banking_account.account_name", bank_code: "$banking.code" },
+                        financial: "$financial",
+                        status: "$status",
+                        status_newmember: "$status_newmember",
+                        create_date: "$create_date",
+                        update_date: "$update_date",
+                        update_by: "$update_by",
+                    }
+                },
+                // {
+                //     $sort: { _id: -1 }
+                // },
+                // {
+                //     $skip: skip
+                // },
+                // {
+                //     $limit: Number(body.range)
+                // }
+               
+
+            ]).toArray()
+            .then(result => resolve(result))
+            .catch(error => reject(error));
+    });
+}
+
 module.exports.findConF = (body) => {
     return new Promise(async (resolve, reject) => {
         await MongoDB.collection(collectionCONFIGURATION)
